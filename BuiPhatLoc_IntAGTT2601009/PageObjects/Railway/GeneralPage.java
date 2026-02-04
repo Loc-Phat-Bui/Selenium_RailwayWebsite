c66255ebaaf00a4a02ea7730f66a9a01dd653c36
Loc-Phat-Bui <phatlocbui@gmail.com>
2026-02-04 16:49:07 +0700

    update logout xpath

--- BuiPhatLoc_IntAGTT2601009/PageObjects/Railway/GeneralPage.java
+++ BuiPhatLoc_IntAGTT2601009/PageObjects/Railway/GeneralPage.java
@@ -8,7 +8,7 @@ import Constant.Constant;
 public class GeneralPage {
 	// Locators
 	private final By tabLogin = By.xpath("//div[@id='menu']//a[@href='/Account/Login.cshtml']");
-	private final By tabLogout = By.xpath("//div[@id='menu']//a[@href='/Account/Logout']");
+	private final By tabLogout = By.xpath("//div[@id='menu']//a[@href='/Account/Logout.cshtml']");
 	private final By tabChangePassword = By.xpath("//div[@id='menu']//a[@href='/Account/ChangePassword.cshtml']");
 	private final By lblWelomeMessage = By.xpath("//div[@class='account']/strong");
 	private final By tabRegister = By.xpath("//div[@id='menu']//a[@href='/Account/Register.cshtml']");
