33d15b218ba8009534c8973f3ee0fe5dc76d8679
Loc-Phat-Bui <phatlocbui@gmail.com>
2026-02-05 16:21:47 +0700

    fixed bug related to xpath

--- BuiPhatLoc_IntAGTT2601009/PageObjects/Railway/RegisterPage.java
+++ BuiPhatLoc_IntAGTT2601009/PageObjects/Railway/RegisterPage.java
@@ -10,11 +10,11 @@ public class RegisterPage extends GeneralPage {
 	// Methods
 	public <T> T register(Account.AccountInfo account, Class<T> returnPage) {
 		
-		SafetyUtilities.safeSendkey(this.getTxtBoxWebElement(Macros.txtboxUsername), account.getUsername());
+		SafetyUtilities.safeSendkey(this.getTxtBoxWebElement(Macros.txtboxEmail), account.getUsername());
 		SafetyUtilities.safeSendkey(this.getTxtBoxWebElement(Macros.txtboxPassword), account.getPassword());
 		SafetyUtilities.safeSendkey(this.getTxtBoxWebElement(Macros.txtboxConfirmPassword), account.getPassword());	
 		SafetyUtilities.safeSendkey(this.getTxtBoxWebElement(Macros.txtboxPID), account.getPID());
-		SafetyUtilities.safeClick(this.getTxtBoxWebElement(Macros.btnRegister));
+		SafetyUtilities.safeClick(this.getBtnWebElement(Macros.btnRegister));
 		
 		try {
 			return returnPage.getDeclaredConstructor().newInstance();
