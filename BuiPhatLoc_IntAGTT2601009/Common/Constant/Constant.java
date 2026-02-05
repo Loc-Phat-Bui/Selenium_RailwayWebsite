cb3125ab46d88bcd8eb94ac46695707c4974117f
Loc-Phat-Bui <phatlocbui@gmail.com>
2026-02-05 13:27:50 +0700

    add Guerrilla mail URL

--- BuiPhatLoc_IntAGTT2601009/Common/Constant/Constant.java
+++ BuiPhatLoc_IntAGTT2601009/Common/Constant/Constant.java
@@ -7,5 +7,6 @@ import Common.Utilities;
 public class Constant {
 	public static WebDriver WEBDRIVER;
 	public static final String RAILWAY_URL="http://saferailway.somee.com";
+	public static final String GUERRILLA_MAIL_URL = "https://www.guerrillamail.com";
 	public static final String CHROME_DRIVER_PATH = Utilities.getProjectPath() + "\\Executables\\chromedriver.exe";
 }
