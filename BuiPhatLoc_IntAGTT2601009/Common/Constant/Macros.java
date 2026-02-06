5a28222cf25a0532d7466ce2b7588298edd486d2
Loc-Phat-Bui <phatlocbui@gmail.com>
2026-02-06 16:45:12 +0700

    change all value

--- BuiPhatLoc_IntAGTT2601009/Common/Constant/Macros.java
+++ BuiPhatLoc_IntAGTT2601009/Common/Constant/Macros.java
@@ -1,32 +1,38 @@
 package Constant;
 
 public class Macros {
-	public static final String TAB_MENU_LOGIN = "tab login";
-	public static final String TAB_MENU_LOGOUT = "tab logout";
-	public static final String TAB_MENU_CHANGE_PASSWORD= "tab change password";
-	public static final String TAB_MENU_REGISTER = "tab register";
-	public static final String TAB_MENU_FAQ = "tab faq";
-	
-	public static final String TAB_CONTENT_REGISTER = "content register";
-	public static final String TAB_CONTENT_FORGOT_PASSWORD = "content forgot password";
-	
-	public static final String TXT_BOX_USERNAME = "username txtbox";
-	public static final String TXT_BOX_EMAIL = "email txtbox";
-	public static final String TXT_BOX_PASSWORD = "password txtbox";
-	public static final String TXT_BOX_CURRENT_PASSWORD = "current password txtbox";
-	public static final String TXT_BOX_NEW_PASSWORD = "new password txtbox";
-	public static final String TXT_BOX_CONFIRM_PASSWORD = "confirm password txtbox";
-	public static final String TXT_BOX_PID = "pid txtbox";
-	
-	public static final String BTN_LOGIN = "login button";
-	public static final String BTN_REGISTER = "register button";
-	public static final String BTN_CHANGE_PASSWORD = "change password button";
-	public static final String BTN_RESET_PASSWORD = "reset password button";
-	public static final String BTN_SEND_INSTRUCTIONS = "send instructions button";
-	
-	public static final String SELECT_DEPART_DATE = "select depart date";
-	public static final String SELECT_DEPART_FROM = "select depart from";
-	public static final String SELECT_ARRIVE_AT = "select arrive at";
-	public static final String SELECT_SEAT_TYPE = "select seat type";
-	public static final String SELECT_TICKET_AMOUNT = "select ticket amount";
+	// Tab Menu - Already matched your first few examples
+    public static final String TAB_MENU_LOGIN = "/Account/Login.cshtml";
+    public static final String TAB_MENU_LOGOUT = "/Account/Logout";
+    public static final String TAB_MENU_CHANGE_PASSWORD = "menu/Account/ChangePassword.cshtml";
+    public static final String TAB_MENU_REGISTER = "/Account/Register.cshtml";
+    public static final String TAB_MENU_FAQ = "/Page/FAQ.cshtml";
+    
+    // Tab Content
+    public static final String TAB_CONTENT_REGISTER = "content/Account/Register.cshtml";
+    public static final String TAB_CONTENT_FORGOT_PASSWORD = "/Account/ForgotPassword.cshtml";
+    
+    // Text Boxes
+    public static final String TXT_BOX_USERNAME = "username";
+    public static final String TXT_BOX_EMAIL = "email";
+    public static final String TXT_BOX_PASSWORD = "password";
+    public static final String TXT_BOX_CURRENT_PASSWORD = "currentPassword";
+    public static final String TXT_BOX_NEW_PASSWORD = "newPassword";
+    public static final String TXT_BOX_CONFIRM_PASSWORD = "confirmPassword";
+    public static final String TXT_BOX_PID = "pid";
+    
+    // Buttons
+    public static final String BTN_LOGIN = "login";
+    public static final String BTN_REGISTER = "Register";
+    public static final String BTN_CHANGE_PASSWORD = "Change Password";
+    public static final String BTN_RESET_PASSWORD = "Reset Password";
+    public static final String BTN_SEND_INSTRUCTIONS = "Send Instructions";
+    public static final String BTN_BOOK_TICKET = "Book ticket";
+    
+    // Selects
+    public static final String SELECT_DEPART_DATE = "Date";
+    public static final String SELECT_DEPART_FROM = "DepartStation";
+    public static final String SELECT_ARRIVE_AT = "ArriveStation";
+    public static final String SELECT_SEAT_TYPE = "SeatType";
+    public static final String SELECT_TICKET_AMOUNT = "TicketAmount";
 }
