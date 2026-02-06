76e1655d87a8098aa5877eabc15b78d05ded4895
Loc-Phat-Bui <phatlocbui@gmail.com>
2026-02-06 16:58:15 +0700

    Optimize Xpath function

--- BuiPhatLoc_IntAGTT2601009/PageObjects/Railway/GeneralPage.java
+++ BuiPhatLoc_IntAGTT2601009/PageObjects/Railway/GeneralPage.java
@@ -39,67 +39,20 @@ public class GeneralPage {
 	**/
 	// ------ Generate Xpath ------ //
 	public String getTabElementXpath (String tabName) {
-		String divPart = "";
-		String hrefPart = "";
-		switch (tabName.toLowerCase()) {
-			case Macros.TAB_MENU_LOGIN: 
-				divPart = "menu";
-				hrefPart = "/Account/Login.cshtml"; 
-				break;
-			case Macros.TAB_MENU_LOGOUT: 
-				divPart = "menu";
-				hrefPart = "/Account/Logout"; 
-				break;
-			case Macros.TAB_MENU_CHANGE_PASSWORD: 
-				divPart = "menu";
-				hrefPart = "/Account/ChangePassword.cshtml"; 
-				break;
-			case Macros.TAB_MENU_REGISTER: 
-				divPart = "menu";
-				hrefPart = "/Account/Register.cshtml"; 
-				break;
-			case Macros.TAB_MENU_FAQ: 
-				divPart = "menu";
-				hrefPart = "/Page/FAQ.cshtml"; 
-				break;
-			case Macros.TAB_CONTENT_REGISTER: 
-				divPart = "content";
-				hrefPart = "/Account/Register.cshtml"; 
-				break;
-			case Macros.TAB_CONTENT_FORGOT_PASSWORD: 
-				divPart = "content";
-				hrefPart = "/Account/ForgotPassword.cshtml";
-				break;
-			default:
-				break;
-		}
+		int index = tabName.indexOf('/');
+	    if (index == -1) {
+	        return String.format(tabElementXpath, tabName, "");
+	    }
+	    String divPart = tabName.substring(0, index);
+	    String hrefPart = tabName.substring(index + 1);
+	    
 		return String.format(tabElementXpath, divPart ,hrefPart);
 	}
 	public String getTxtBoxElemnentXpath (String txtxboxName) {
-		String hrefPart = "";
-		switch (txtxboxName.toLowerCase()) {
-			case Macros.TXT_BOX_USERNAME: hrefPart = "username"; break;
-			case Macros.TXT_BOX_EMAIL: hrefPart = "email"; break;
-			case Macros.TXT_BOX_PASSWORD: hrefPart = "password"; break;
-			case Macros.TXT_BOX_CONFIRM_PASSWORD: hrefPart = "confirmPassword"; break;
-			case Macros.TXT_BOX_PID: hrefPart = "pid"; break;
-			case Macros.TXT_BOX_CURRENT_PASSWORD: hrefPart = "currentPassword"; break;
-			case Macros.TXT_BOX_NEW_PASSWORD: hrefPart = "newPassword"; break;
-			default: break;
-		}
-		return String.format(txtboxElementXpath, hrefPart);
+		return String.format(txtboxElementXpath, txtxboxName);
 	}
 	public String getBtnElemnentXpath (String btnName) {
-		String hrefPart = "";
-		switch (btnName.toLowerCase()) {
-			case Macros.BTN_LOGIN: hrefPart = "login"; break;
-			case Macros.BTN_REGISTER: hrefPart = "Register"; break;
-			case Macros.BTN_CHANGE_PASSWORD: hrefPart = "Change Password"; break;
-			case Macros.BTN_RESET_PASSWORD: hrefPart = "Reset Password"; break; 
-			case Macros.BTN_SEND_INSTRUCTIONS: hrefPart = "Send Instructions"; break;
-			default: break;
-		}
-		return String.format(btnElementXpath, hrefPart);
+		return String.format(btnElementXpath, btnName);
 	}
 	
 	// ------ Check Functions ------ //
