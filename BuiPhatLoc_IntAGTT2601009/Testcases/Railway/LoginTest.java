56eaa202bd0207c0a919b461e21ee718638a7ba4
Loc-Phat-Bui <phatlocbui@gmail.com>
2026-02-06 12:38:17 +0700

    comment all testcase to test other testcase

--- BuiPhatLoc_IntAGTT2601009/Testcases/Railway/LoginTest.java
+++ BuiPhatLoc_IntAGTT2601009/Testcases/Railway/LoginTest.java
@@ -10,151 +10,151 @@ import Constant.Macros;
 public class LoginTest extends BaseTestMethod{
 	private SoftAssert softAssert = new SoftAssert();
 	
-	@Test
-	public void TC01() {
-		System.out.println("TC1 - User can log into Railway with valid username and password");
-		
-		this.createValidAccount();
-		
-		System.out.println("Step: 1. Navigate to QA Railway Website");
-		System.out.println("Step: 2. Click on \"Login\" tab");
-		
-		LoginPage loginPage = homePage.gotoTabPage(Macros.tabLogin, LoginPage.class);
-		
-		System.out.println("Step: 3. Enter valid Email and Password");
-		System.out.println("Step: 4. Click on \"Login\" button");
-		
-		loginPage.login(Account.getAccountInfo("TC1"), HomePage.class);
-		
-		System.out.println("Verify: User is logged into Railway. Welcome user message is displayed.");
-		
-		String actualString = homePage.getWelcomeMessageString();
-		String expectedString = "Welcome " + Account.getAccountInfo("TC1").getUsername();		
-		Assert.assertEquals(actualString, expectedString, "Welcome message is not displayed as expected");
-	}
-	
-	@Test
-	public void TC02() {
-		System.out.println("TC2 - User cannot login with blank \"Username\" textbox");
-		
-		System.out.println("Step: 1. Navigate to QA Railway Website");
-		System.out.println("Step: 2. Click on \"Login\" tab");
-		
-		LoginPage loginPage = homePage.gotoTabPage(Macros.tabLogin, LoginPage.class);
-		
-		System.out.println("Step: 3. User doesn't type any words into \"Username\" textbox but enter valid information into \"Password\" textbox ");
-		System.out.println("Step: 4. Click on \"Login\" button");
-		
-		loginPage.login(Account.getAccountInfo("TC2"), HomePage.class);
-		
-		System.out.println("Verify: User can't login and message \"There was a problem with your login and/or errors exist in your form.\" appears.");
-		
-		String actualString = loginPage.getLblLoginErrorMsgText();
-		String expectedString = "There was a problem with your login and/or errors exist in your form.";
-		Assert.assertEquals(actualString, expectedString, "Error Message is not displayed as expected");
-	}
-	
-	@Test
-	public void TC03() {
-		System.out.println("TC3 - User cannot log into Railway with invalid password ");
-		
-		System.out.println("Step: 1. Navigate to QA Railway Website");
-		System.out.println("Step: 2. Click on \"Login\" tab");
-		
-		LoginPage loginPage = homePage.gotoTabPage(Macros.tabLogin, LoginPage.class);
-		
-		System.out.println("Step: 3. Enter valid Email and invalid ");
-		System.out.println("Step: 4. Click on \"Login\" button");
-		
-		loginPage.login(Account.getAccountInfo("TC3"), HomePage.class);
-		
-		System.out.println("Verify: Error message \"There was a problem with your login and/or errors exist in your form.\" is displayed");
-		
-		String actualString = loginPage.getLblLoginErrorMsgText();
-		String expectedString = "There was a problem with your login and/or errors exist in your form.";
-		Assert.assertEquals(actualString, expectedString, "Error Message is not displayed as expected");
-	}
-	
-	@Test
-	public void TC04() {
-		System.out.println("TC4 - System shows message when user enters wrong password many times");
-		
-		System.out.println("Step: 1. Navigate to QA Railway Website");
-		System.out.println("Step: 2. Click on \"Login\" tab");
-		
-		LoginPage loginPage = homePage.gotoTabPage(Macros.tabLogin, LoginPage.class);
-		
-		System.out.println("Step: 3. Enter valid information into \"Username\" textbox except \"Password\" textbox.");
-		System.out.println("Step: 4. Click on \"Login\" button");
-		
-		loginPage.login(Account.getAccountInfo("TC4"), HomePage.class);
-		
-		System.out.println("Verify: \"Invalid username or password. Please try again\" is shown");
-		
-		String actualString = loginPage.getLblLoginErrorMsgText();
-		String expectedString = "Invalid username or password. Please try again.";
-		softAssert.assertEquals(actualString, expectedString, "Attempt - 1: Error Message is not displayed as expected");
-		
-		System.out.println("Step: 5. Repeat step 3 and 4 three more times.");
-		
-		for(int i = 2; i <= 4; i++) {
-			loginPage.login(Account.getAccountInfo("TC4"), HomePage.class);
-			if (i <= 3) {
-				actualString = loginPage.getLblLoginErrorMsgText();
-				softAssert.assertEquals(actualString, expectedString, "Attempt - "+ i + ": Error Message is not displayed as expected");
-			}
-		}
-		softAssert.assertAll();
-		
-		System.out.println("Verify: User can't login and message \"You have used 4 out of 5 login attempts. After all 5 have been used, you will be unable to login for 15 minutes.\" appears.");
-
-		actualString = loginPage.getLblLoginErrorMsgText();
-		expectedString = "You have used 4 out of 5 login attempts. After all 5 have been used, you will be unable to login for 15 minutes.";
-		Assert.assertEquals(actualString, expectedString, "Error Message is not displayed as expected");
-	}
-	
-	@Test
-	public void TC05() {
-		System.out.println("TC5 - User can't login with an account hasn't been activated");
-		System.out.println("Pre-condition: a not-active account is existing");
-
-	    RegisterPage registerPage = homePage.gotoTabPage(Macros.tabRegister, RegisterPage.class);
-	    registerPage.register(Account.getAccountInfo("TC5"), HomePage.class);
-		
-		System.out.println("Step: 1. Navigate to QA Railway Website");
-		System.out.println("Step: 2. Click on \"Login\" tab");
-		
-		LoginPage loginPage = homePage.gotoTabPage(Macros.tabLogin, LoginPage.class);
-		
-		System.out.println("Step: 3. Enter username and password of account hasn't been activated.");
-		System.out.println("Step: 4. Click on \"Login\" button");
-		
-		loginPage.login(Account.getAccountInfo("TC5"), HomePage.class);
-		
-		System.out.println("Verify: User can't login and message \"Invalid username or password. Please try again.\" appears.");
-		
-		String actualString = loginPage.getLblLoginErrorMsgText();
-		String expectedString = "Invalid username or password. Please try again.";
-		Assert.assertEquals(actualString, expectedString, "Error Message is not displayed as expected");
-	}
-	
-	@Test
-	public void TC06() {
-		System.out.println("TC6 - User is redirected to Home page after logging out");
-		
-		System.out.println("Step: 1. Navigate to QA Railway Website");
-		System.out.println("Step: 2. Login with valid Email and Password");
-		
-		LoginPage loginPage = homePage.gotoTabPage(Macros.tabLogin, LoginPage.class);
-		loginPage.login(Account.getAccountInfo("TC1"), HomePage.class);
-		
-		System.out.println("Step: 3. Click on \"FAQ\" ");
-		System.out.println("Step: 4. Click on \"Log out\" ");
-		
-		homePage.gotoTabPage(Macros.tabFAQ, FAQPage.class);
-		homePage.gotoTabPage(Macros.tabLogout, HomePage.class);
-		
-		System.out.println("Verify: Home page displays. \"Log out\" tab is disappeared.");
-		Assert.assertTrue(!homePage.checkTabElementAvailable(Macros.tabLogout));
-	}
+//	@Test
+//	public void TC01() {
+//		System.out.println("TC1 - User can log into Railway with valid username and password");
+//		
+//		this.createValidAccount();
+//		
+//		System.out.println("Step: 1. Navigate to QA Railway Website");
+//		System.out.println("Step: 2. Click on \"Login\" tab");
+//		
+//		LoginPage loginPage = homePage.gotoTabPage(Macros.tabLogin, LoginPage.class);
+//		
+//		System.out.println("Step: 3. Enter valid Email and Password");
+//		System.out.println("Step: 4. Click on \"Login\" button");
+//		
+//		loginPage.login(Account.getAccountInfo("TC1"), HomePage.class);
+//		
+//		System.out.println("Verify: User is logged into Railway. Welcome user message is displayed.");
+//		
+//		String actualString = homePage.getWelcomeMessageString();
+//		String expectedString = "Welcome " + Account.getAccountInfo("TC1").getUsername();		
+//		Assert.assertEquals(actualString, expectedString, "Welcome message is not displayed as expected");
+//	}
+//	
+//	@Test
+//	public void TC02() {
+//		System.out.println("TC2 - User cannot login with blank \"Username\" textbox");
+//		
+//		System.out.println("Step: 1. Navigate to QA Railway Website");
+//		System.out.println("Step: 2. Click on \"Login\" tab");
+//		
+//		LoginPage loginPage = homePage.gotoTabPage(Macros.tabLogin, LoginPage.class);
+//		
+//		System.out.println("Step: 3. User doesn't type any words into \"Username\" textbox but enter valid information into \"Password\" textbox ");
+//		System.out.println("Step: 4. Click on \"Login\" button");
+//		
+//		loginPage.login(Account.getAccountInfo("TC2"), HomePage.class);
+//		
+//		System.out.println("Verify: User can't login and message \"There was a problem with your login and/or errors exist in your form.\" appears.");
+//		
+//		String actualString = loginPage.getLblLoginErrorMsgText();
+//		String expectedString = "There was a problem with your login and/or errors exist in your form.";
+//		Assert.assertEquals(actualString, expectedString, "Error Message is not displayed as expected");
+//	}
+//	
+//	@Test
+//	public void TC03() {
+//		System.out.println("TC3 - User cannot log into Railway with invalid password ");
+//		
+//		System.out.println("Step: 1. Navigate to QA Railway Website");
+//		System.out.println("Step: 2. Click on \"Login\" tab");
+//		
+//		LoginPage loginPage = homePage.gotoTabPage(Macros.tabLogin, LoginPage.class);
+//		
+//		System.out.println("Step: 3. Enter valid Email and invalid ");
+//		System.out.println("Step: 4. Click on \"Login\" button");
+//		
+//		loginPage.login(Account.getAccountInfo("TC3"), HomePage.class);
+//		
+//		System.out.println("Verify: Error message \"There was a problem with your login and/or errors exist in your form.\" is displayed");
+//		
+//		String actualString = loginPage.getLblLoginErrorMsgText();
+//		String expectedString = "There was a problem with your login and/or errors exist in your form.";
+//		Assert.assertEquals(actualString, expectedString, "Error Message is not displayed as expected");
+//	}
+//	
+//	@Test
+//	public void TC04() {
+//		System.out.println("TC4 - System shows message when user enters wrong password many times");
+//		
+//		System.out.println("Step: 1. Navigate to QA Railway Website");
+//		System.out.println("Step: 2. Click on \"Login\" tab");
+//		
+//		LoginPage loginPage = homePage.gotoTabPage(Macros.tabLogin, LoginPage.class);
+//		
+//		System.out.println("Step: 3. Enter valid information into \"Username\" textbox except \"Password\" textbox.");
+//		System.out.println("Step: 4. Click on \"Login\" button");
+//		
+//		loginPage.login(Account.getAccountInfo("TC4"), HomePage.class);
+//		
+//		System.out.println("Verify: \"Invalid username or password. Please try again\" is shown");
+//		
+//		String actualString = loginPage.getLblLoginErrorMsgText();
+//		String expectedString = "Invalid username or password. Please try again.";
+//		softAssert.assertEquals(actualString, expectedString, "Attempt - 1: Error Message is not displayed as expected");
+//		
+//		System.out.println("Step: 5. Repeat step 3 and 4 three more times.");
+//		
+//		for(int i = 2; i <= 4; i++) {
+//			loginPage.login(Account.getAccountInfo("TC4"), HomePage.class);
+//			if (i <= 3) {
+//				actualString = loginPage.getLblLoginErrorMsgText();
+//				softAssert.assertEquals(actualString, expectedString, "Attempt - "+ i + ": Error Message is not displayed as expected");
+//			}
+//		}
+//		softAssert.assertAll();
+//		
+//		System.out.println("Verify: User can't login and message \"You have used 4 out of 5 login attempts. After all 5 have been used, you will be unable to login for 15 minutes.\" appears.");
+//
+//		actualString = loginPage.getLblLoginErrorMsgText();
+//		expectedString = "You have used 4 out of 5 login attempts. After all 5 have been used, you will be unable to login for 15 minutes.";
+//		Assert.assertEquals(actualString, expectedString, "Error Message is not displayed as expected");
+//	}
+//	
+//	@Test
+//	public void TC05() {
+//		System.out.println("TC5 - User can't login with an account hasn't been activated");
+//		System.out.println("Pre-condition: a not-active account is existing");
+//
+//	    RegisterPage registerPage = homePage.gotoTabPage(Macros.tabRegister, RegisterPage.class);
+//	    registerPage.register(Account.getAccountInfo("TC5"), HomePage.class);
+//		
+//		System.out.println("Step: 1. Navigate to QA Railway Website");
+//		System.out.println("Step: 2. Click on \"Login\" tab");
+//		
+//		LoginPage loginPage = homePage.gotoTabPage(Macros.tabLogin, LoginPage.class);
+//		
+//		System.out.println("Step: 3. Enter username and password of account hasn't been activated.");
+//		System.out.println("Step: 4. Click on \"Login\" button");
+//		
+//		loginPage.login(Account.getAccountInfo("TC5"), HomePage.class);
+//		
+//		System.out.println("Verify: User can't login and message \"Invalid username or password. Please try again.\" appears.");
+//		
+//		String actualString = loginPage.getLblLoginErrorMsgText();
+//		String expectedString = "Invalid username or password. Please try again.";
+//		Assert.assertEquals(actualString, expectedString, "Error Message is not displayed as expected");
+//	}
+//	
+//	@Test
+//	public void TC06() {
+//		System.out.println("TC6 - User is redirected to Home page after logging out");
+//		
+//		System.out.println("Step: 1. Navigate to QA Railway Website");
+//		System.out.println("Step: 2. Login with valid Email and Password");
+//		
+//		LoginPage loginPage = homePage.gotoTabPage(Macros.tabLogin, LoginPage.class);
+//		loginPage.login(Account.getAccountInfo("TC1"), HomePage.class);
+//		
+//		System.out.println("Step: 3. Click on \"FAQ\" ");
+//		System.out.println("Step: 4. Click on \"Log out\" ");
+//		
+//		homePage.gotoTabPage(Macros.tabFAQ, FAQPage.class);
+//		homePage.gotoTabPage(Macros.tabLogout, HomePage.class);
+//		
+//		System.out.println("Verify: Home page displays. \"Log out\" tab is disappeared.");
+//		Assert.assertTrue(!homePage.checkTabElementAvailable(Macros.tabLogout));
+//	}
 }
