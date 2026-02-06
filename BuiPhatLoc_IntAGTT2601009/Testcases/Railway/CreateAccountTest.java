97c3b97cccf55fd62fbd2dac7009e53f5233ab5a
Loc-Phat-Bui <phatlocbui@gmail.com>
2026-02-06 12:43:29 +0700

    create Register and ChangePassword test class

---
+++ BuiPhatLoc_IntAGTT2601009/Testcases/Railway/CreateAccountTest.java
@@ -0,0 +1,125 @@
+package Railway;
+
+import javax.crypto.Mac;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import Account.Account;
+import Common.Utilities;
+import Constant.Constant;
+import Constant.Macros;
+import Guerrillamail.GuerrillaMailPage;
+
+public class CreateAccountTest extends BaseTestMethod {
+	private SoftAssert softAssert = new SoftAssert();
+	
+//	@Test
+//	public void TC07() {
+//		System.out.println("TC7 - User can't create account with an already in-use email");
+//		
+//		Account.AccountInfo account =  Account.getAccountInfo("TC7");
+//		
+//		System.out.println("Pre-condition: an actived account is existing");
+//		
+//		System.out.println(account.getUsername());
+//		this.createValidAccount(account);
+//		
+//		System.out.println("Step: 1. Navigate to QA Railway Website");
+//		System.out.println("Step: 2. Click on \"Register\" tab");
+//		
+//		RegisterPage registerPage = homePage.gotoTabPage(Macros.tabRegister, RegisterPage.class);
+//		
+//		System.out.println("Step: 3. Enter information of the created account in Pre-condition");
+//		System.out.println("Step: 4. Click on \"Register\" button");
+//		
+//		registerPage.register(account, HomePage.class);
+//		
+//		System.out.println("Verify: Error message \"This email address is already in use.\" displays above the form.");
+//		
+//		String actualString = registerPage.getLblRegisterErrorMSGText();
+//		String expectedString = "This email address is already in use.";
+//		Assert.assertEquals(actualString, expectedString, "Error Message is not displayed as expected");
+//	}
+	
+//	@Test
+//	public void TC08() {
+//		System.out.println("TC8 - User can't create account while password and PID fields are empty");
+//		
+//		Account.AccountInfo account =  Account.getAccountInfo("TC8");
+//		
+//		System.out.println("Step: 1. Navigate to QA Railway Website");
+//		System.out.println("Step: 2. Click on \"Register\" tab");
+//		
+//		RegisterPage registerPage = homePage.gotoTabPage(Macros.TAB_MENU_REGISTER, RegisterPage.class);
+//		
+//		System.out.println("Step: 3. Enter valid email address and leave other fields empty");
+//		System.out.println("Step: 4. Click on \"Register\" button");
+//		
+//		registerPage.register(account, HomePage.class);
+//		
+//		System.out.println("Verify: Message \"There're errors in the form. Please correct the errors and try again.\" appears above the form.");
+//		
+//		String actualString = registerPage.getLblRegisterErrorMSGText();
+//		String expectedString = "There're errors in the form. Please correct the errors and try again.";
+//		softAssert.assertEquals(actualString, expectedString, "Error Message is not displayed as expected");
+//		
+//		System.out.println("Verify: Next to password fields, error message \"Invalid password length\" displays");
+//		
+//		actualString = registerPage.getLblRegisterInvalidPasswordMSGText();
+//		expectedString = "Invalid password length";
+//		softAssert.assertEquals(actualString, expectedString, "Error Message is not displayed as expected");
+//		
+//		System.out.println("Verify: Next to PID field, error message \"Invalid ID length\" displays");
+//		
+//		actualString = registerPage.getLblRegisterInvalidPIDMSGText();
+//		expectedString = "Invalid ID length";
+//		softAssert.assertEquals(actualString, expectedString, "Error Message is not displayed as expected");
+//		
+//		softAssert.assertAll();
+//	}
+	
+	@Test
+	public void TC09() {
+		System.out.println("TC9 - User create and activate account");
+		Account.AccountInfo account = Account.getAccountInfo("TC9");
+		
+		System.out.println("Step: 1. Navigate to QA Railway Website");
+		System.out.println("Verify	: Home page is shown with guide containing href \"create an account\" to \"Register\" page");
+		softAssert.assertTrue(homePage.checkTabElementAvailable(Macros.TAB_HOME_REGISTER));
+		
+		System.out.println("Step: 2. Click on \"Create an account\"");
+		RegisterPage registerPage = homePage.gotoTabPage(Macros.TAB_HOME_REGISTER, RegisterPage.class);
+		
+		System.out.println("Verify: Register page is shown");
+		softAssert.assertTrue(homePage.checkPageURL(Macros.TAB_MENU_REGISTER));
+		
+		System.out.println("Step: 3. Enter valid information into all fields");
+		System.out.println("Step: 4. Click on \"Register\" button");
+		registerPage.register(account, HomePage.class);
+		
+		System.out.println("Verify: \"Thank you for registering your account\" is shown");
+		String actualString = registerPage.getTextRegisterContentHeaderText();
+		String expectedString = "Thank you for registering your account";
+		softAssert.assertEquals(actualString, expectedString, "Text is not displayed as expected");
+		
+		System.out.println("Step: 5. Get email information (webmail address, mailbox and password) and navigate to that webmail ");
+		System.out.println("Step: 6. Login to the mailbox");
+		Utilities.switchToNewTab(Constant.GUERRILLA_MAIL_URL);
+		GuerrillaMailPage guerrillamailPage = new GuerrillaMailPage();
+		guerrillamailPage.checkGuerillaEmail(account);
+		
+		System.out.println("Step: 7. Open email with subject containing \"Please confirm your account\"  and the email of the new account at step 3");
+		System.out.println("Step: 8. Click on the activate link");
+		guerrillamailPage.checkConfirmEmail();
+	    for (String handle : Constant.WEBDRIVER.getWindowHandles()) {
+	        Constant.WEBDRIVER.switchTo().window(handle);
+	    }
+		
+		System.out.println("Verify: Redirect to Railways page and message \"Registration Confirmed! You can now log in to the site\" is shown");
+		actualString = registerPage.getTextRegisterContentParagraphText();
+		expectedString = "Registration Confirmed! You can now log in to the site";
+		softAssert.assertEquals(actualString, expectedString, "Text is not displayed as expected");
+	}
+}
---
+++ BuiPhatLoc_IntAGTT2601009/Testcases/Railway/ResetPassword.java
@@ -0,0 +1,13 @@
+package Railway;
+
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class ResetPassword {
+	SoftAssert softAssert = new SoftAssert();
+	
+	@Test
+	public void TC10() {
+		
+	}
+}
