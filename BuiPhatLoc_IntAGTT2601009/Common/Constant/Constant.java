445a87b32a6d281e5c785ba1c38f2e426867b60b
Loc-Phat-Bui <phatlocbui@gmail.com>
2026-02-11 15:56:11 +0700

    add firefox driver

--- BuiPhatLoc_IntAGTT2601009/Common/Constant/Constant.java
+++ BuiPhatLoc_IntAGTT2601009/Common/Constant/Constant.java
@@ -9,4 +9,5 @@ public class Constant {
 	public static final String RAILWAY_URL="http://saferailway.somee.com";
 	public static final String GUERRILLA_MAIL_URL = "https://www.guerrillamail.com";
 	public static final String CHROME_DRIVER_PATH = Utilities.getProjectPath() + "\\Executables\\chromedriver.exe";
+	public static final String FIREFOX_DRIVER_PATH = Utilities.getProjectPath() + "\\Executables\\geckodriver.exe";
 }
