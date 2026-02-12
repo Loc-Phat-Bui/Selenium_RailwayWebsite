6d22523ece943ac36cb2aff0c8440f55fe1e644a
Loc-Phat-Bui <phatlocbui@gmail.com>
2026-02-12 10:29:58 +0700

    add checkTicketPrice()

--- BuiPhatLoc_IntAGTT2601009/PageObjects/Railway/TicketPricePage.java
+++ BuiPhatLoc_IntAGTT2601009/PageObjects/Railway/TicketPricePage.java
@@ -4,14 +4,27 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
 import Common.WaitUtilities;
+import Constant.Constant;
+import Datas.TicketPrice;
 
 public class TicketPricePage {
 	// Locators
 	private final By tableTicketPrice = By.xpath("//table[@class='MyTable MedTable']//tr[contains(.,'Price (VND)')]");
+	private final String ticketPriceXpath = "//td[contains(text(),'%s')]/following-sibling::td[contains(text(),'%s')]/following-sibling::td[contains(text(),'%s')]/following-sibling::td[contains(text(),'%s')]/following-sibling::td[contains(text(),'%s')]/..";
 	// Elements
 	protected WebElement getTableTicketPriceWebElement() {
 		return WaitUtilities.waitForElementVisible(tableTicketPrice);
 	}
 	// Methods
-	
+	public boolean checkTicketPrice(TicketPrice.TicketPriceInfo ticketPrice) {
+		By xPath = By.xpath(String.format(
+				ticketPriceXpath,
+				ticketPrice.getPriceHS(),
+				ticketPrice.getPriceSS(),
+				ticketPrice.getPriceSSC(),
+				ticketPrice.getPriceHB(),
+				ticketPrice.getPriceSB(),
+				ticketPrice.getPriceSBC()));
+		return !Constant.WEBDRIVER.findElements(xPath).isEmpty();
+	}
 }
