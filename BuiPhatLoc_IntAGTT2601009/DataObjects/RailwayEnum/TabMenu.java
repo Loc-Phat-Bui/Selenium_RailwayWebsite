442ef45147208802aeb96160bd2d9bbf35eb3522
Loc-Phat-Bui <phatlocbui@gmail.com>
2026-02-20 18:12:28 +0700

    change value in enum

--- BuiPhatLoc_IntAGTT2601009/DataObjects/RailwayEnum/TabMenu.java
+++ BuiPhatLoc_IntAGTT2601009/DataObjects/RailwayEnum/TabMenu.java
@@ -1,14 +1,14 @@
 package RailwayEnum;
 
 public enum TabMenu {
-	LOGIN("menu", "/Account/Login.cshtml"),
-    LOGOUT("menu", "/Account/Logout"),
-    CHANGE_PASSWORD("menu", "/Account/ChangePassword.cshtml"),
-    REGISTER("menu", "/Account/Register.cshtml"),
-    FAQ("menu", "/Page/FAQ.cshtml"),
-    BOOK_TICKET("menu", "/Page/BookTicketPage.cshtml"),
-    TIMETABLE("menu", "/TrainTimeListPage.cshtml"),
-    MY_TICKET("menu", "/Page/ManageTicket.cshtml");
+	LOGIN("menu", "Account/Login.cshtml"),
+    LOGOUT("menu", "Account/Logout"),
+    CHANGE_PASSWORD("menu", "Account/ChangePassword.cshtml"),
+    REGISTER("menu", "Account/Register.cshtml"),
+    FAQ("menu", "Page/FAQ.cshtml"),
+    BOOK_TICKET("menu", "Page/BookTicketPage.cshtml"),
+    TIMETABLE("menu", "TrainTimeListPage.cshtml"),
+    MY_TICKET("menu", "Page/ManageTicket.cshtml");
 
 	private final String div;
     private final String href;
