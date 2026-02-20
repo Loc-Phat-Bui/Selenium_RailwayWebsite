cf074d41a1232e176421032d2edc32cef24934c5
Loc-Phat-Bui <phatlocbui@gmail.com>
2026-02-20 18:14:22 +0700

    Refactor Xpath generator, now use to generate Locator, fix related functions

--- BuiPhatLoc_IntAGTT2601009/PageObjects/Railway/GeneralPage.java
+++ BuiPhatLoc_IntAGTT2601009/PageObjects/Railway/GeneralPage.java
@@ -13,54 +13,68 @@ import RailwayEnum.TextBox;
 import RailwayEnum.ValidationError;
 
 public class GeneralPage {
-	private String tabElementXpath = "//div[@id='%s']//a[contains(@href,'%s')]";
+	private String tabElementXpath = "//div[@id='%s']//a[contains(text(),'%s')]";
 	private String txtboxElementXpath = "//input[@id='%s']";
 	private String btnElementXpath	=	"//input[@value='%s']";
 	private String labelValidationErrorXpath = "//label[contains(@for,'%s') and contains(@class,'validation-error')]";
 	/* 
 	** Locators
 	*/
+	// ------ Static Locator ------ //
 	private final By lblWelomeMessage = By.xpath("//div[@class='account']/strong");
 	private final By lblErrorMSG = By.xpath("//p[@class='message error']");
-	// ------ Generate Xpath ------ //
-	public String getTabElementXpath (TabMenu tabMenu) {
-		return String.format(tabElementXpath, tabMenu.getDiv() ,tabMenu.getHref());
-	}
-	public String getTabElementXpath (TabContent tabContent) {
-		return String.format(tabElementXpath, tabContent.getDiv() ,tabContent.getHref());
-	}
-	public String getTxtBoxElemnentXpath (TextBox txtBox) {
-		return String.format(txtboxElementXpath, txtBox.getValue());
-	}
-	public String getBtnElemnentXpath (Button btnName) {
-		return String.format(btnElementXpath, btnName.getValue());
-	}
-	public String getLabelValidationErrorXpath (ValidationError labelName) {
-	    
-		return String.format(labelValidationErrorXpath, labelName.getValue());
-	}
+	// ------ Dynamic Locator ------ //
+	private By getTabLocator(TabMenu tabMenu) {
+        String tmpXpath = String.format(tabElementXpath, tabMenu.getDiv(), tabMenu.getHref());
+        return By.xpath(tmpXpath);
+    }
+
+    private By getTabLocator(TabContent tabContent) {
+        String tmpXpath = String.format(tabElementXpath, tabContent.getDiv(), tabContent.getHref());
+        return By.xpath(tmpXpath);
+    }
+
+    private By getTxtBoxLocator(TextBox txtBox) {
+        String tmpXpath = String.format(txtboxElementXpath, txtBox.getValue());
+        return By.xpath(tmpXpath);
+    }
+
+    private By getBtnLocator(Button btnName) {
+        String tmpXpath = String.format(btnElementXpath, btnName.getValue());
+        return By.xpath(tmpXpath);
+    }
+
+    private By getLabelValidationErrorLocator(ValidationError labelName) {
+        String tmpXpath = String.format(labelValidationErrorXpath, labelName.getValue());
+        return By.xpath(tmpXpath);
+    }
 	/* 
 	** Elements
 	*/
 	// General elements
-	protected WebElement getTabWebElement(TabMenu tabMenu) {	
-		return WaitUtilities.waitForElementClickable(By.xpath(getTabElementXpath(tabMenu)));
-	}
-	protected WebElement getTabWebElement(TabContent tabContent) {	
-		return WaitUtilities.waitForElementClickable(By.xpath(getTabElementXpath(tabContent)));
-	}
-	protected WebElement getTxtBoxWebElement (TextBox txtBox) {
-		return WaitUtilities.waitForElementVisible(By.xpath(getTxtBoxElemnentXpath(txtBox)));
-	}
-	protected WebElement getBtnWebElement (Button btnName) {
-		return WaitUtilities.waitForElementClickable(By.xpath(getBtnElemnentXpath(btnName)));
-	}
-	protected WebElement getLabelValidationErrorWebElement(ValidationError labelName) {
-		return  WaitUtilities.waitForElementClickable(By.xpath(getLabelValidationErrorXpath(labelName)));
-	}
-	protected WebElement getLblErrorMSGWebElement() {
-		return WaitUtilities.waitForElementVisible(lblErrorMSG);
-	}
+	protected WebElement getTabWebElement(TabMenu tabMenu) {
+        return WaitUtilities.waitForElementClickable(getTabLocator(tabMenu));
+    }
+
+    protected WebElement getTabWebElement(TabContent tabContent) {
+        return WaitUtilities.waitForElementClickable(getTabLocator(tabContent));
+    }
+
+    protected WebElement getTxtBoxWebElement(TextBox txtBox) {
+        return WaitUtilities.waitForElementVisible(getTxtBoxLocator(txtBox));
+    }
+
+    protected WebElement getBtnWebElement(Button btnName) {
+        return WaitUtilities.waitForElementClickable(getBtnLocator(btnName));
+    }
+
+    protected WebElement getLabelValidationErrorWebElement(ValidationError labelName) {
+        return WaitUtilities.waitForElementVisible(getLabelValidationErrorLocator(labelName));
+    }
+
+    protected WebElement getLblErrorMSGWebElement() {
+        return WaitUtilities.waitForElementVisible(lblErrorMSG);
+    }
 	// Specific elements
 	protected WebElement getLblWelcomeMessageWebElement() {
 		return Constant.WEBDRIVER.findElement(this.lblWelomeMessage);
@@ -70,18 +84,19 @@ public class GeneralPage {
 	*/
 	// ------ Check Functions ------ //
 	public boolean checkTabElementAvailable(TabMenu tabMenu) {
-		return !Constant.WEBDRIVER.findElements(By.xpath(getTabElementXpath(tabMenu))).isEmpty();
-	}
-	public boolean checkTabElementAvailable(TabContent tabContent) {
-		return !Constant.WEBDRIVER.findElements(By.xpath(getTabElementXpath(tabContent))).isEmpty();
-	}
-	public boolean checkPageURL(TabMenu tabMenu) {
-		WebElement tab = getTabWebElement(tabMenu);
-		String expectedHref = tab.getAttribute("href");
-		String pageURL = Constant.WEBDRIVER.getCurrentUrl();
-		
-		return pageURL.contains(expectedHref);
-	}
+        return !Constant.WEBDRIVER.findElements(getTabLocator(tabMenu)).isEmpty();
+    }
+
+    public boolean checkTabElementAvailable(TabContent tabContent) {
+        return !Constant.WEBDRIVER.findElements(getTabLocator(tabContent)).isEmpty();
+    }
+
+    public boolean checkPageURL(TabMenu tabMenu) {
+        WebElement tab = getTabWebElement(tabMenu);
+        String expectedHref = tab.getAttribute("href");
+        String pageURL = Constant.WEBDRIVER.getCurrentUrl();
+        return pageURL.contains(expectedHref);
+    }
 	
 	// ------ Goto Page by clicking Tab Menu/Content link ------ //
 	public <T> T gotoTabPage (TabMenu tabMenu, Class<T> returnPage) {
@@ -105,9 +120,10 @@ public class GeneralPage {
 	
 	// ------ Get text from the website ------ //
 	public String getWelcomeMessageString() {
-		return this.getLblWelcomeMessageWebElement().getText();	
-	}
-	public String getLabelValidationErrorText(ValidationError labelName) {
-		return getLabelValidationErrorWebElement(labelName).getText();
-	}
+        return this.getLblWelcomeMessageWebElement().getText();
+    }
+
+    public String getLabelValidationErrorText(ValidationError labelName) {
+        return getLabelValidationErrorWebElement(labelName).getText();
+    }
 }
