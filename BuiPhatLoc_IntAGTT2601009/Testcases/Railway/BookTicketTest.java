279cfbc1ac4c7ac04a6201b39259696bf72c82d2
Loc-Phat-Bui <phatlocbui@gmail.com>
2026-02-20 18:13:25 +0700

    change comment Step: 4. Select the next 2 days from \"Depart date\ to Step: 4. Select the next 25 days from \"Depart date\

--- BuiPhatLoc_IntAGTT2601009/Testcases/Railway/BookTicketTest.java
+++ BuiPhatLoc_IntAGTT2601009/Testcases/Railway/BookTicketTest.java
@@ -76,7 +76,7 @@ public class BookTicketTest extends TestBase{
 		System.out.println("Step: 3. Click on \"Book ticket\" tab");
 		bookTicketPage = homePage.gotoTabPage(TabMenu.BOOK_TICKET, BookTicketPage.class);
 		
-		System.out.println("Step: 4. Select the next 5 days from \"Depart date\"");
+		System.out.println("Step: 4. Select the next 25 days from \"Depart date\"");
 		System.out.println("Step: 5. Select Depart from \"Nha Trang\" and Arrive at \"Huế\"");
 		System.out.println("Step: 6. Select \"Soft bed with air conditioner\" for \"Seat type\"");
 		System.out.println("Step: 7. Select \"1\" for \"Ticket amount\"");
