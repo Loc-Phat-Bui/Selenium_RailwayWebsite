81b0efc065e1c2f06596cc42e497417a42808b29
Loc-Phat-Bui <phatlocbui@gmail.com>
2026-02-20 18:15:44 +0700

    refactor code that generate Locator instead of Xpath String

--- BuiPhatLoc_IntAGTT2601009/PageObjects/Railway/MyTicketPage.java
+++ BuiPhatLoc_IntAGTT2601009/PageObjects/Railway/MyTicketPage.java
@@ -5,22 +5,23 @@ import org.openqa.selenium.WebElement;
 
 import Common.SafetyUtilities;
 import Common.WaitUtilities;
+import Constant.Constant;
 import RailwayDatas.Ticket;
 
 public class MyTicketPage extends GeneralPage {
+	private String myTicketTableItemXpath = "//td[contains(text(),'%s')]/following-sibling::td[contains(text(),'%s')]/following-sibling::td[contains(text(),'%s')]/following-sibling::td[contains(text(),'%s')]/following-sibling::td[contains(text(),'%s')]/input";
 	// Locators
-	private String myTicketTableItemXpath = "//td[contains(text(),'%s')]/following-sibling::td[contains(text(),'%s')]/following-sibling::td/input";
+	public By getMyTicketTableItemXpath(Ticket.TicketInfo ticket) {
+		return By.xpath(String.format(myTicketTableItemXpath, ticket.getDepartFrom(), ticket.getArriveAt()));
+	}
 	// Elements
-	protected WebElement getMyTicketTableItemWebElement(String departFrom, String arriveAt) {
-		return WaitUtilities.waitForElementClickable(By.xpath(getMyTicketTableItemXpath(departFrom, arriveAt)));
+	protected WebElement getMyTicketTableItemWebElement(Ticket.TicketInfo ticket) {
+		return WaitUtilities.waitForElementClickable(getMyTicketTableItemXpath(ticket));
 	}
 	// Methods
-	public String getMyTicketTableItemXpath(String departFrom, String arriveAt) {
-		return String.format(myTicketTableItemXpath, departFrom, arriveAt);
-	}
 	public <T> T cancelMyTicket(Ticket.TicketInfo ticket, Class<T> returnPage) {
 		
-		SafetyUtilities.safeClick(getMyTicketTableItemWebElement(ticket.getDepartFrom(), ticket.getArriveAt()));
+		SafetyUtilities.safeClick(getMyTicketTableItemWebElement(ticket));
 		
 		WaitUtilities.waitForAlert().accept();
 		
@@ -29,5 +30,9 @@ public class MyTicketPage extends GeneralPage {
 		} catch (Exception e) {
 			throw new RuntimeException("Could not create instance of " + returnPage);
 		}
+	}
+	
+	public boolean isTicketExist(Ticket.TicketInfo ticket) {
+		return Constant.WEBDRIVER.findElements(getMyTicketTableItemXpath(ticket)).isEmpty();
 	}
 }
